5bcace5ee52a6e35e841e6ffead439b9a6a7d109
Seamus Mullan <43112447+SeamusMullan@users.noreply.github.com>
2024-09-25 15:43:45 +0100

    Header classes for processing

--- source/PluginProcessor.cpp
+++ source/PluginProcessor.cpp
@@ -10,12 +10,13 @@ PluginProcessor::PluginProcessor()
                       #endif
                        .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
                      #endif
-                       )
+                       ), apvts(*this, nullptr, "Parameters", createParameterLayout())
 {
 }
 
 PluginProcessor::~PluginProcessor()
 {
+    // destroy anything created ig
 }
 
 //==============================================================================
@@ -86,8 +87,21 @@ void PluginProcessor::changeProgramName (int index, const juce::String& newName)
 //==============================================================================
 void PluginProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
+    juce::dsp::ProcessSpec spec{};
+    spec.sampleRate = sampleRate;
+    spec.numChannels = samplesPerBlock;
+    spec.numChannels = getTotalNumInputChannels();
+
+    preLP.prepare(spec);
+    preHP.prepare(spec);
+
+    // haasDelay.reset();
+    haasDelay.prepare(spec);
+    chorus.prepare(spec);
+    convolution.prepare(spec);
+
+    compressor.prepare(spec);
+
     juce::ignoreUnused (sampleRate, samplesPerBlock);
 }
 
@@ -128,26 +142,15 @@ void PluginProcessor::processBlock (juce::AudioBuffer<float>& buffer,
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
+
     for (int channel = 0; channel < totalNumInputChannels; ++channel)
     {
         auto* channelData = buffer.getWritePointer (channel);
         juce::ignoreUnused (channelData);
-        // ..do something to the data...
+        // .do something to the data.
     }
 }
 
--- source/PluginProcessor.h
+++ source/PluginProcessor.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_dsp/juce_dsp.h>
 
 #if (MSVC)
 #include "ipps.h"
@@ -38,6 +39,72 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
+    juce::AudioProcessorValueTreeState apvts;
+
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout()
+    {
+        /*
+        Parameters List
+
+        Pre-LP & HP
+            - Filter Type
+            - Cutoff Freq
+
+        Multiband Pre-Gain
+            - Low Gain
+            - Mid Gain
+            - High Gain
+
+        Haas Delay
+            - Time
+            - Mix
+
+        Chorus
+            - Rate
+            - Depth
+            - Centre Delay
+            - Feedback
+            - Mix
+
+        Waveshaper
+            - n/a
+
+        Convolution
+            - Impulse Response
+            - Mix
+
+        Compressor
+            - Threshold
+            - Ratio
+            - Attack
+            - Release
+            - Mix
+
+        */
+
+        std::vector<std::unique_ptr<juce::RangedAudioParameter>> params;
+
+        params.push_back(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID{"IN", 1}, "In Gain", -60.0f, 10.0f, 0.0f));
+        params.push_back(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID{"OUT", 1}, "Out Gain", -60.0f, 10.0f, 0.0f));
+
+        params.push_back(std::make_unique<juce::AudioParameterBool>(juce::ParameterID{"BYPASS", 1}, "Bypass", false));
+
+        return { params.begin(), params.end() };
+    }
+
 private:
+
+    juce::dsp::Compressor<float> compressor;
+
+    juce::dsp::LinkwitzRileyFilter<float> preLP, preHP;
+    juce::dsp::Gain<float> lowGain, midGain, highGain;
+
+    juce::dsp::DelayLine<float> haasDelay; // 5-35  ms
+    juce::dsp::Chorus<float> chorus;
+    // juce::dsp::WaveShaper<float> waveshaper;
+    juce::dsp::Convolution<float> convolution; // ir for cabinets or reverb?
+
+
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginProcessor)
 };
